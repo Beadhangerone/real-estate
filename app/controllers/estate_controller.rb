36aeb2e0d5fcfefe2fe18e63007b3de1fa5154ab
Beadhangerone <denm.2206@gmail.com>
2017-01-05 23:24:14 +0200

    search by 3 params done

--- app/controllers/estate_controller.rb
+++ app/controllers/estate_controller.rb
@@ -1,4 +1,37 @@
 class EstateController < ApplicationController
 	def index
+		state = params[:state]
+		region = params[:region]
+		bedrooms = params[:bedrooms]
+		
+		if state and region
+			@estates = Estate.where(state: state, region: region)
+		elsif state and bedrooms
+			@estates = Estate.where(state: state, bedrooms: bedrooms)
+		elsif state
+			@estates = Estate.where(state: state)
+		end		
+
+		# if not region and bedrooms
+		# 	@estates = Estate.where(state: state)
+		# elsif not bedrooms
+		# 	@estates = Estate.where(state: state, region: region)
+		# elsif not region
+		# 	@estates = Estate.where(state: state, bedrooms: bedrooms)
+		# else
+		# 	@estates = Estate.where(state: state, region: region, bedrooms: bedrooms)
+		# end
+
+
+
+		# if region == 'Any' and bedrooms == 'Any'
+		# 	@estates = Estate.where(state: state)
+		# elsif bedrooms == 'Any'
+		# 	@estates = Estate.where(state: state, region: region)
+		# elsif region == 'Any'
+		# 	@estates = Estate.where(state: state, bedrooms: bedrooms)
+		# else
+		# 	@estates = Estate.where(state:state, region: region, bedrooms: bedrooms)
+		# end
 	end
 end
--- db/migrate/20170105192416_create_estates.rb
+++ db/migrate/20170105192416_create_estates.rb
@@ -1,7 +1,13 @@
 class CreateEstates < ActiveRecord::Migration[5.0]
   def change
     create_table :estates do |t|
-
+      t.text :title
+      t.text :state
+      t.text :region
+      t.integer :bedrooms
+      t.boolean :sell
+      t.boolean :rent
+      t.integer :price
       t.timestamps
     end
   end
