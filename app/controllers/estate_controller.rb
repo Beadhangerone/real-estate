72b03f8f5e583a7bec241c6cd7b8fd94cfd7f9a3
Beadhangerone <denm.2206@gmail.com>
2017-01-06 10:46:47 +0200

    custom headers done

--- app/controllers/estate_controller.rb
+++ app/controllers/estate_controller.rb
@@ -1,15 +1,25 @@
 class EstateController < ApplicationController
 	def index
+		@msg = {} #header message
+		@msg[:h1] = 'Find Estate!'
+		@msg[:h2] = 'You are in the right place'
+		@msg[:slogan] = 'We help you to find the best real estate offers from the whole United States!'
+
 		state = params[:state]
 		region = params[:region]
 		bedrooms = params[:bedrooms]
+
 		
-		if state and region
+		if state and region and bedrooms
+			@estates = Estate.where(state: state, region: region, bedrooms: bedrooms)
+		elsif state and region
 			@estates = Estate.where(state: state, region: region)
 		elsif state and bedrooms
 			@estates = Estate.where(state: state, bedrooms: bedrooms)
 		elsif state
 			@estates = Estate.where(state: state)
+		else 
+			@estates = Estate.all
 		end		
 	end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
 
 	def index
-		
+		@msg = {} #header message
+		@msg[:h1] = 'Welcome!'
+		@msg[:h2] = 'You are in the right place'
+		@msg[:slogan] = 'We help you to find the best real estate offers from the whole United States!'
+		@msg[:btn] = {:href => estate_index_path, :text=> 'Find Real Estate for Me!'}
 	end
 end
